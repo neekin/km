3d1c4a92b39d570600326177be77bd5ead74628a
NeeKin <neekin@NeeKindeMini.lan>
2025-04-23 23:43:51 +0800

    十分钟后自动让客户端离线

--- licence/tasks.py
+++ licence/tasks.py
@@ -3,5 +3,5 @@ from datetime import timedelta
 from .models import Licence
 
 def set_offline():
-    ten_minutes_ago = timezone.now() - timedelta(minutes=1)
+    ten_minutes_ago = timezone.now() - timedelta(minutes=10)
     Licence.objects.filter(update_time__lt=ten_minutes_ago).update(status=0)
