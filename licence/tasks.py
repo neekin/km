be942bef0b0ef1e38a96815c1922754f548ecc90
NeeKin <neekin@NeeKindeMini.lan>
2025-04-24 00:15:29 +0800

    自动运行添加日志

--- licence/tasks.py
+++ licence/tasks.py
@@ -1,7 +1,11 @@
+import logging
 from django.utils import timezone
 from datetime import timedelta
 from .models import Licence
 
+logger = logging.getLogger(__name__)
+
 def set_offline():
-    ten_minutes_ago = timezone.now() - timedelta(minutes=10)
-    Licence.objects.filter(update_time__lt=ten_minutes_ago).update(status=0)
+    ten_minutes_ago = timezone.now() - timedelta(minutes=1)
+    updated_count = Licence.objects.filter(update_time__lt=ten_minutes_ago).update(status=0)
+    logger.info(f"set_offline executed. Updated {updated_count} records.")
